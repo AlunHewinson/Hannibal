1a3f2f8025c6dc4f9cf2a2f66de4020c3cb6a5c9
alunhewinson <alunhewinson@gmail.com>
2021-01-02 10:59:21 +0000

    quieten messages down from this script

--- data/read_GFit_data.R
+++ data/read_GFit_data.R
@@ -1,22 +1,27 @@
-library(RGoogleFit)
-library(magrittr)
-library(tidyverse)
-library(glue)
-#library(sp)
-#library(leaflet)
-#library(lubridate)
+library(RGoogleFit, quietly = TRUE)
+library(magrittr, quietly = TRUE)
+library(tidyverse, quietly = TRUE)
+library(glue, quietly = TRUE)
+library(leaflet, quietly = TRUE)
+library(lubridate, quietly = TRUE)
 
 options(RGoogleFit.client_id=Sys.getenv("GFIT_CLIENT_ID"),
         RGoogleFit.client_secret=Sys.getenv("GFIT_CLIENT_SECRET"))
 
 gfit_token <- GetFitOauth2Token()
 
-## distance from steps, or so it seems!
-GFit_distance <- GetFitDataset(
+quiet <- function(x) {
+  sink(tempfile())
+  on.exit(sink())
+  invisible(force(x))
+}
+
+## distance from Google Fit
+GFit_distance <- quiet(GetFitDataset(
   gfit_token,
   "derived:com.google.distance.delta:com.google.android.gms:merge_distance_delta",
   as.POSIXct("2021-01-01 00:00:00"), as.POSIXct("2021-12-31 23:59:59")
-)$point %>% as_tibble()
+))$point %>% as_tibble()
 GFit_distance$value2 <- sapply(GFit_distance$value, function(q) q$fpVal)
 GFit_distance %<>% mutate(start = NanosToPOSIXct(startTimeNanos),
                   end = NanosToPOSIXct(endTimeNanos),
@@ -25,7 +30,4 @@ GFit_distance %<>% mutate(start = NanosToPOSIXct(startTimeNanos),
 
 daily_dist <- GFit_distance %>% 
   group_by(date) %>% 
-  summarise(distance=last(cumvalue))
-
-glue("Distance travelled this year: {round(max(daily_dist$distance)/1000, 3)}km") %>%
-  message()
+  summarise(distance=last(cumvalue), .groups="drop")
